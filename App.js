897a85ed8367e8e8430ff88e44e604c46b4759f0
Evandro Pomatti <TECBMEPI@TECLOGICA.COM.BR>
2019-03-23 15:03:21 -0300

    fonts

--- App.js
+++ App.js
@@ -2,30 +2,36 @@ import React from 'react'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
-//import DeckList from './components/DeckList'
-import { Container, Header, Content, Button, Text } from 'native-base'
+import DeckList from './components/DeckList'
+import { Header, Content, Root, Container } from 'native-base'
 import { Font } from 'expo'
 import { Ionicons } from '@expo/vector-icons'
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: true };
+  }
   async componentDidMount() {
     await Font.loadAsync({
       Roboto: require('native-base/Fonts/Roboto.ttf'),
       Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
       ...Ionicons.font,
     })
+    this.setState({ loading: false });
   }
   render() {
+    const { loading } = this.state
     return (
       <Provider store={createStore(reducer)}>
-        <Container>
-          <Header />
-          <Content>
-            <Button>
-              <Text>Create Deck</Text>
-            </Button>
-          </Content>
-        </Container>
+        <Root>
+          {loading === false && (
+            <Container>
+              <Header />
+              <Content>
+                <DeckList />
+              </Content></Container>)}
+        </Root>
       </Provider>
     );
   }
